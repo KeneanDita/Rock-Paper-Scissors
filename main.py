09a5df8e79a278a260a20a5cb050e90da4c0a496
Kenean <keneansufa@gmail.com>
2024-12-03 04:00:16 +0000

    Win and Tie

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import random
+from turtledemo.nim import computerzug
+
+
 def play():
     user = input("What's your choice? 'r' for rock, 'p' for paper, 's' scissors\n")
     user = user.lower()
 
     computer = random.choice(['r', 'p', 's'])
+    if user == computer:
+        return "You and the computer have chosen {}. It's a Tie".format(computer)
+    if isWin(user, computer):
+        return "You have chosen {} and the computer has chosen {}. You Won".format(user, computer)
+def isWin(player, opponent):
+    if(player == 'r' and opponent=='s') or (player=='s'and opponent=='p') or (player=='p'and opponent=='r'):
+        return True
+    return False
+
+
+if __name__ == '__main__':
+    print(play())
